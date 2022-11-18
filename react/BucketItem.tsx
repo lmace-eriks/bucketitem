f22808bb37762c6694999111f6ec593f83af74e8
Web Developer <useraccount@WebDeveloperMBP.local>
2022-11-18 16:49:59 -0600

    V1

--- react/BucketItem.tsx
+++ react/BucketItem.tsx
@@ -5,21 +5,85 @@ import { canUseDOM } from "vtex.render-runtime";
 import styles from "./styles.css";
 
 interface BucketItemProps {
-
+  desktopImage: string
+  mobileImage: string
+  title: string
+  subtitle: string
+  link: string
+  blockClass: string
 }
 
-const BucketItem: StorefrontFunctionComponent<BucketItemProps> = ({ }) => {
-  // const [openGate, setOpenGate] = useState(true);
+const BucketItem: StorefrontFunctionComponent<BucketItemProps> = ({ desktopImage, mobileImage, title, subtitle, link, blockClass }) => {
+  const [siteIsAdmin, setSiteIsAdmin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const mobileImageMaxSize = useMemo(() => 50000, []);
 
   useEffect(() => {
-    console.clear();
+    if (!canUseDOM) return;
 
-  })
+    const userIsAdmin = window.location.href.includes("siteEditor=true");
 
+    if (userIsAdmin) {
+      setSiteIsAdmin(userIsAdmin);
+      getMobileImage();
+    }
+  });
 
-  return (
-    <div>Hello World</div>
+  const bytesToKb = (b: number) => b / 1000;
+
+  const getMobileImage = () => {
+    if (!mobileImage) {
+      setErrorMessage("Mobile Image Not Set.");
+      return;
+    }
+    const tempMobileImage = new Image();
+    tempMobileImage.src = mobileImage;
+
+    const xmlRequest = new XMLHttpRequest();
+    xmlRequest.open("GET", mobileImage);
+    xmlRequest.responseType = "blob";
+
+    xmlRequest.onload = () => {
+      const blob = xmlRequest.response;
+      verifyMobileImage(blob.size);
+    }
+    xmlRequest.send();
+  }
+
+  const verifyMobileImage = (imageSize: number) => {
+    const fileSizePassed = imageSize <= mobileImageMaxSize;
+
+    // Mobile Image file size is not too large
+    if (!fileSizePassed) {
+      setErrorMessage(`Mobile Image File Size Too Large. Maximum is ${bytesToKb(mobileImageMaxSize)} Kb. Image provided is ${bytesToKb(imageSize)} Kb.`);
+      return;
+    }
+
+    setErrorMessage("");
+  }
+
+  const ValidBucket = () => (
+    <a href={link} className={`${styles.bucketContainer}--${blockClass}`}>
+      <div className={`${styles.imageContainer}--${blockClass}`}>
+        <picture>
+          <source media="(min-width:1026px)" srcSet={desktopImage} />
+          <source media="(max-width:1025px)" srcSet={mobileImage} />
+          <img src={mobileImage} alt={`${title}.${subtitle ? " " + subtitle + "." : ""}`} loading="lazy" className={`${styles.image}--${blockClass}`} />
+        </picture>
+      </div>
+      <div className={`${styles.textContainer}--${blockClass}`}>
+        <div className={`${styles.title}--${blockClass}`}>{title}</div>
+        {subtitle && <div className={`${styles.subtitle}--${blockClass}`}>{subtitle}</div>}
+      </div>
+    </a>
   )
+
+  const ErrorMessage = () => (
+    <div className={styles.errorMessage}>{errorMessage}</div>
+  )
+
+  return !siteIsAdmin ? <ValidBucket /> : errorMessage ? <ErrorMessage /> : <ValidBucket />
 }
 
 BucketItem.schema = {
@@ -37,15 +101,10 @@ BucketItem.schema = {
       type: "string",
       description: "Optional | Sub Title Text."
     },
-    ctaText: {
-      title: "Call To Action Text",
+    link: {
+      title: "Link",
       type: "string",
-      description: "Required | Button Text."
-    },
-    ctaLink: {
-      title: "Call To Action Link",
-      type: "string",
-      description: "Required | Button Link."
+      description: "Required | Relative or Absolute Path"
     },
     desktopImage: {
       title: "Desktop Image Path",
