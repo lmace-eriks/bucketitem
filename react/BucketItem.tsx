d9ecfad50ec04062d76b8ee496cf2c1943dd5e18
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-05-25 14:31:45 -0400

    Removed size warnings. Removed picture tag. Updated Site Editor - LM

--- react/BucketItem.tsx
+++ react/BucketItem.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { canUseDOM } from "vtex.render-runtime";
 
 // Styles
 import styles from "./styles.css";
@@ -10,90 +9,22 @@ interface BucketItemProps {
   title: string
   subtitle: string
   link: string
+  altText: string
   blockClass: string
-  desktopWidth: number
-  desktopHeight: number
-  mobileWidth: number
-  mobileHeight: number
 }
 
-const BucketItem: StorefrontFunctionComponent<BucketItemProps> = ({ desktopImage, mobileImage, title, subtitle, link, blockClass, desktopWidth, desktopHeight, mobileWidth, mobileHeight }) => {
-  const [siteIsAdmin, setSiteIsAdmin] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const userDevice = useRef("");
+const BucketItem: StorefrontFunctionComponent<BucketItemProps> = ({ desktopImage, mobileImage, title, subtitle, link, altText, blockClass }) => {
 
-  const mobileImageMaxSize = useMemo(() => 50000, []);
-
-  desktopWidth = desktopWidth || 500;
-  desktopHeight = desktopHeight || 500;
-  mobileWidth = mobileWidth || 350;
-  mobileHeight = mobileHeight || 350;
-
-  useEffect(() => {
-    if (!canUseDOM) return;
-    getWindowSize();
-    const userIsAdmin = window.location.href.includes("siteEditor=true");
-
-    if (userIsAdmin) {
-      setSiteIsAdmin(userIsAdmin);
-      getMobileImage();
-    }
-  });
-
-  const getWindowSize = () => {
-    if (!canUseDOM) return;
-
-    const windowWidth = window.innerWidth;
-    userDevice.current = windowWidth <= 1025 ? "mobile" : "desktop";
-  }
-
-  const bytesToKb = (b: number) => b / 1000;
-
-  const getMobileImage = () => {
-    if (!mobileImage) {
-      setErrorMessage("Mobile Image Not Set.");
-      return;
-    }
-    const tempMobileImage = new Image();
-    tempMobileImage.src = mobileImage;
-
-    const xmlRequest = new XMLHttpRequest();
-    xmlRequest.open("GET", mobileImage);
-    xmlRequest.responseType = "blob";
-
-    xmlRequest.onload = () => {
-      const blob = xmlRequest.response;
-      verifyMobileImage(blob.size);
-    }
-    xmlRequest.send();
-  }
-
-  const verifyMobileImage = (imageSize: number) => {
-    const fileSizePassed = imageSize <= mobileImageMaxSize;
-
-    // Mobile Image file size is not too large
-    if (!fileSizePassed) {
-      setErrorMessage(`Mobile Image File Size Too Large. Maximum is ${bytesToKb(mobileImageMaxSize)} Kb. Image provided is ${bytesToKb(imageSize)} Kb.`);
-      return;
-    }
-
-    setErrorMessage("");
-  }
-
-  const imageSize = (dim: string) => {
-    return "" + userDevice.current === "mobile" ? dim === "w" ? mobileWidth : mobileHeight : dim === "w" ? desktopWidth : desktopHeight;
-  }
-
-  const ValidBucket = () => (
+  return (
     <a href={link} className={`${styles.bucketContainer}--${blockClass}`}>
       <div className={`${styles.imageContainer}--${blockClass}`}>
-        <picture>
-          {/* @ts-expect-error -- width and height do not appear in the definition for <source> yet - LM */}
-          <source media="(min-width:1026px)" srcSet={desktopImage} width={imageSize("w")} height={imageSize("h")} />
-          {/* @ts-expect-error */}
-          <source media="(max-width:1025px)" srcSet={mobileImage} width={imageSize("w")} height={imageSize("h")} />
-          <img src={mobileImage} alt={`${title}.${subtitle ? " " + subtitle + "." : ""}`} loading="lazy" className={`${styles.image}--${blockClass}`} width={imageSize("w")} height={imageSize("h")} />
-        </picture>
+        <img src={mobileImage}
+          srcSet={`${desktopImage} 500w, ${mobileImage} 300w`}
+          sizes="(min-width: 1026px) 500px, 300px"
+          loading="lazy"
+          width={350} height={350}
+          alt={title || altText || ""}
+          className={`${styles.image}--${blockClass}`} />
       </div>
       <div className={`${styles.textContainer}--${blockClass}`}>
         {title && <div className={`${styles.title}--${blockClass}`}>{title}</div>}
@@ -101,12 +32,6 @@ const BucketItem: StorefrontFunctionComponent<BucketItemProps> = ({ desktopImage
       </div>
     </a>
   )
-
-  const ErrorMessage = () => (
-    <div className={styles.errorMessage}>{errorMessage}</div>
-  )
-
-  return !siteIsAdmin ? <ValidBucket /> : errorMessage ? <ErrorMessage /> : <ValidBucket />
 }
 
 BucketItem.schema = {
@@ -114,54 +39,40 @@ BucketItem.schema = {
   description: "",
   type: "object",
   properties: {
+    desktopImage: {
+      title: "Desktop Image- 500 x 500 - 250KB",
+      type: "string",
+      description: "Required | .jpg only | Absolute Path.",
+      widget: { "ui:widget": "image-uploader" }
+    },
+    mobileImage: {
+      title: "Mobile Image - 350 x 350 - 50KB",
+      type: "string",
+      widget: { "ui:widget": "image-uploader" }
+    },
+    altText: {
+      title: "Alt Text",
+      type: "string",
+      description: "Required if Title and Sub Title are blank.",
+      widget: { "ui:widget": "textarea" }
+    },
     title: {
       title: "Title",
       type: "string",
-      description: "Required | Title Text."
+      description: "Optional",
+      widget: { "ui:widget": "textarea" }
     },
     subtitle: {
       title: "Sub Title",
       type: "string",
-      description: "Optional | Sub Title Text."
+      description: "Optional",
+      widget: { "ui:widget": "textarea" }
     },
     link: {
       title: "Link",
       type: "string",
-      description: "Required | Relative or Absolute Path"
-    },
-    desktopImage: {
-      title: "Desktop Image Path",
-      type: "string",
-      description: "Required | .jpg only | Absolute Path."
-    },
-    desktopWidth: {
-      title: "Desktop Image Width",
-      type: "number",
-      description: "",
-      default: 500
-    },
-    desktopHeight: {
-      title: "Desktop Image Height",
-      type: "number",
-      description: "",
-      default: 500
-    },
-    mobileImage: {
-      title: "Mobile Image Path",
-      type: "string",
-      description: "Required | .jpg only | Absolute Path."
-    },
-    mobileWidth: {
-      title: "Mobile Image Width",
-      type: "number",
-      description: "",
-      default: 350
-    },
-    mobileHeight: {
-      title: "Mobile Image Height",
-      type: "number",
-      description: "",
-      default: 350
+      description: "Required | Relative or Absolute Path",
+      widget: { "ui:widget": "textarea" }
     }
   }
 }
